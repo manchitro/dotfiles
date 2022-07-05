8fd6b722d9bcdf6bbfd5a8eb2293ccb96829c2a4
Sazid Uddin <initialsaremsu@gmail.com>
2022-07-05 18:25:12 +0600

    beautified qtile config file

--- scripts/link_sys_files.sh
+++ scripts/link_sys_files.sh
@@ -2,5 +2,6 @@
 
 sudo ln dotroot/etc/vimrc /etc/vimrc;
 sudo ln dotroot/etc/systemd/system/cool.service /etc/systemd/system/cool.service
+sudo ln dotroot/etc/sudoers.d/isw /etc/sudoers.d/isw
 
 exit 0;
