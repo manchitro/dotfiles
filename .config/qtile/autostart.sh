756b303bfbb12a9953759927927a944888ecc1ff
Sazid Uddin <initialsaremsu@gmail.com>
2022-07-15 14:43:11 +0600

    added yay installed list

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -13,6 +13,7 @@ blueman-applet &
 kdeconnect-cli -l &
 protonvpn &
 xbacklight -set 70 &
+pactl -- set-sink-volume @DEFAULT_SINK@ 70%
 sh /home/s/scripts/generate_installed_list.sh &
 sudo isw -b on
 deadd-notification-center &
--- scripts/generate_installed_list.sh
+++ scripts/generate_installed_list.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-pacman -Qe > /home/s/.installed-packages
+pacman -Qe > /home/s/.installed-packages-pac
+pacman -Qm > /home/s/.installed-packages-aur
